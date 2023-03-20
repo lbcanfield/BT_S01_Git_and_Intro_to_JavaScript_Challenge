6fa4bbfd34d9749e3b53fde30c24f2ecad84b3de
lbcanfield <lewie.b.canfield@gmail.com>
2023-03-20 08:56:38 -0500

    function addArtist completed.

--- index.js
+++ index.js
@@ -292,8 +292,17 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-     /*Your Code Here*/
+function addArtist(array) {
+     const newArtist = {
+          id: 20,
+          name: 'Your Name Here',
+          years: 'Your Birth Year - current day',
+          genre: 'Web Design',
+          nationality: 'Your Nationality Here',
+          bio: 'Add 1 - 2 sentences(or use lorem ipsum)'
+     };
+     array.push(newArtist);
+     return array;
 }
 
 
