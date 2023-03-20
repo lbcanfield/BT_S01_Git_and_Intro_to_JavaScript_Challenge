2d0ff8100cee6f546a247e0e7f74878ed13ada60
lbcanfield <lewie.b.canfield@gmail.com>
2023-03-20 11:46:04 -0500

    Function addArtist completed.

--- index.js
+++ index.js
@@ -287,8 +287,17 @@ Use addArtist to do the following:
 5. Add the newly created object to the copied array, then return the copied array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-     /*Your Code Here*/
+function addArtist(array, strName, strYears, strGenre, strNationality, strBio) {
+     let tmpArray = [...array];
+     let newArtist = {
+          name: strName,
+          years: strYears,
+          genre: strGenre,
+          nationality: strNationality,
+          bio: strBio
+     };
+     tmpArray.push(newArtist);
+     return tmpArray;
 }
 
 
